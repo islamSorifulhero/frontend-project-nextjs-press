c0f4ddff08c8288aa05bd6919185c7214dade00a
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-07-16 15:47:59 +0600

    Dislike Button and slug layout added

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,11 +1,14 @@
+import DislikeButton from '@/app/ui/DislikeButton';
 import React from 'react'
 
-const BlogsSlugPage = () => {
-  return (
-    <div>
-      BlogsSlugPage
-    </div>
-  )
+const BlogsSlugPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+
+  const { slug } = await params;
+  return
+  <div>
+    BlogsSlugPage : {slug}
+    <DislikeButton blogSlug={slug} />
+  </div>
 }
 
 export default BlogsSlugPage
