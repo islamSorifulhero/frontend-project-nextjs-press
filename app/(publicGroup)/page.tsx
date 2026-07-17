35832858ca911e3ae7e75344e9a47e7e396c85bd
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-07-18 00:30:26 +0600

    file and folder structure updatrd

---
+++ app/(publicGroup)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      Hello, News.js!
+
+      <Button size={"xs"} variant={"destructive"}>Click Me</Button>
+    </div>
+  )
+}
+
+export default HomePage
