ef3ede1ade5469604a5af06b6964d0f17651dea9
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-07-19 16:42:01 +0600

    navbar component and login layout added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import LoginPage from "./(authGroup)/login/page";
+import { Navbar } from "@/components/ui/shared/navbar";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -15,15 +17,14 @@ export default async function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={cn("h-full antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
-
-        <Toaster position="top-right" richColors />
-        {/* Navbar */}
-        {children}
-
-        {/* Footer */}
+        <Toaster position="top-right" richColors></Toaster>
+        <Navbar />
+        {/* <Navbar></Navbar> */}
+        <LoginPage></LoginPage>
       </body>
     </html>
   );
