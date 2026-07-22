c0a18886769da25fce6ccc8424870fa45c8dd73b
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-07-22 22:58:53 +0600

    Proxy and error handler added

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+    console.log(request, "request");
+    console.log("Proxy");
+    return NextResponse.redirect(new URL('/', request.url))
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+export const config = {
+    matcher: '/dashboard/:path*',
+}
